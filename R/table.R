1c41a8a906ccf4f122a0a7f09ce49e2057ab59ec
Eli Miller <emiller@AR-RStudioDev.4ygzttwrp44enh1pozggg5gc4g.cx.internal.cloudapp.net>
2020-05-19 20:54:31 +0000

    Updates to table constructor

--- R/table.R
+++ R/table.R
@@ -2,68 +2,98 @@
 
 #' Safety Table
 #'
-#' @param table Source data used to create the table
+#' @description
+#' The function `tplyr_table()` creates a 'tplyr_table' object which is composed
+#' of a table and table display options, and logic for creating the table. The
+#' layer environments passed to a 'tplyr_table' are used to evaluate the code
+#' used to create the table.
+#'
+#' @section Environment Bindings/Properties:
+#' \itemize{
+#' \item{table - The main piece of data the table will be created from.}
+#' \item{headers - The column headers for the table. These will be repeated
+#'   across pages as appropriate.}
+#' \item{header_n - The population count information used as the denominator
+#'   and display in the column headers.}
+#' \item{layers - The logic used to prepare and render the table.}
+#' }
+#'
+#' @param table Source data used to create the table. A 'data.frame' object.
 #' @param headers Column display headers
-#' @param header_n Population count information to dispaly in headers
-#' @param layers Layering components used in table display
+#' @param header_n Population count information to dispaly in headers. A
+#'   data.frame with population level information
 #'
-#' @return A safetyTable environment object
+#' @return A safety_table object which is an environment where the code
+#'   creating the table is evaluated.
 #'
-#' @export
+#' @import rlang
+#' @seealso [layer()]
 #'
 #' @examples
-safety_table <- function(table, headers, header_n, layers) {
+#' df <- data.frame(a = 1:10, b = 11:20)
+#' headers <- c("a", "b")
+#' header_n <- c(a = "10", b = "10")
+#'
+#' tplyr_table(df, headers, header_n)
+#' @export
+tplyr_table <- function(table, headers, header_n) {
   if(missing(table)){
-    return(structure(environment(),
-           class = "safetyTable"))
+    #return a blank environment if no table information is passed
+    return(structure(rlang::env(),
+           class = c("tplyr_table", "environment")))
   }
-  new_saftey_table(table)
-}
-
-#' @noRd
-new_saftey_table <- function(table, headers, header_n, layers) {
-  validate_safety_table(table, headers, header_n, layers)
-
-  structre(environment(something),
-           class = "safetyTable")
+  new_tplyr_table(table, headers, header_n)
 }
 
 #' @import rlang
 #' @noRd
-validate_safety_table <- function(table, headers, header_n, layers) {
+new_tplyr_table <- function(table, headers, header_n) {
+  validate_tplyr_table(table, headers, header_n)
+
+  structure(rlang::env(
+    table = table,
+    headers = headers,
+    header_n = header_n
+  ),
+           class = append("tplyr_table", "environment"))
+}
+
+#' @import rlang
+#' @noRd
+validate_tplyr_table <- function(table, headers, header_n) {
 
   # table should be a data.frame
   if(!inherits(table, "data.frame")) {
-    msg <- paste0("'table' argument passed to safety_table must be a data.frame,",
+    msg <- paste0("'table' argument passed to tplyr_table must be a data.frame,",
                   "\n",
                   "instead a class of: '",
                   class(table),
                   "' was passed.")
     rlang::abort(message = msg,
-                 class = "safety_table_constructor_error")
+                 class = "tplyr_table_constructor_error")
   }
 
   # headers should be a character vector
   if(!inherits(headers, "character")) {
-    msg <- paste0("'headers' argument passed to safety_table must be a data.frame,",
+    msg <- paste0("'headers' argument passed to tplyr_table must be a data.frame,",
                   "\n",
                   "instead a class of: '",
                   class(headers),
                   "' was passed.")
     rlang::abort(message = msg,
-                 class = "safety_table_constructor_error")
+                 class = "tplyr_table_constructor_error")
   }
 
   # header_n should be a named character vector with the names matching the
   # headers
   if(!inherits(header_n, "character")) {
-    msg <- paste0("'header_n' argument passed to safety_table must be a data.frame,",
+    msg <- paste0("'header_n' argument passed to tplyr_table must be a character vector,",
                   "\n",
                   "instead a class of: '",
                   class(header_n),
                   "' was passed.")
     rlang::abort(message = msg,
-                 class = "safety_table_constructor_error")
+                 class = "tplyr_table_constructor_error")
   }
   if(length(header_n) != length(headers)) {
     msg <- paste0("'header_n' argument must be the same length as the 'headers' argument,",
@@ -74,10 +104,10 @@ validate_safety_table <- function(table, headers, header_n, layers) {
                   "length(headers): ",
                   length(headers))
     rlang::abort(message = msg,
-                 class = "safety_table_constructor_error")
+                 class = "tplyr_table_constructor_error")
   }
-  if(!all(names(header_n) %in% headers) &
-     !all(headers %in% header_n)) {
+  if(!(all(names(header_n) %in% headers) &
+     all(headers %in% names(header_n)))) {
     msg <- paste0("'header_n' argument must have same names as the 'headers' argument values,",
                   "\n",
                   "names(header_n): ",
@@ -86,18 +116,7 @@ validate_safety_table <- function(table, headers, header_n, layers) {
                   "headers: ",
                   headers)
     rlang::abort(message = msg,
-                 class = "safety_table_constructor_error")
-  }
-
-  # layers should be safety_table_layers
-  if(!all(inherits(layers, "safety_table_layers"))) {
-    msg <- paste0("'layers' argument passed to safety_table must be a safety_table_layers,",
-                  "\n",
-                  "instead a class of: '",
-                  class(layers),
-                  "' was passed.")
-    rlang::abort(message = msg,
-                 class = "safety_table_constructor_error")
+                 class = "tplyr_table_constructor_error")
   }
 }
 
--- tests/testthat/test-table.R
+++ tests/testthat/test-table.R
@@ -1,4 +1,24 @@
-test_that("safety_table returns an empty envrionment of class 'safety_table' when passed no arguemnts", {
-  st <- safety_table()
+test_that("tplyr_table returns an empty envrionment of class 'tplyr_table' when passed no arguemnts", {
+  st <- tplyr_table()
   expect_true(is.environment(st))
+})
+
+### Errors ###
+test_that("tplyr_table throws error when passed a bad table argument", {
+  expect_error(tplyr_table(matrix(), c("a", "b", "c"), c(a = "1", b = "2", c = "3")), class = "tplyr_table_constructor_error")
+  expect_silent(tplyr_table(data.frame(), c("a", "b", "c"), c(a = "1", b = "2", c = "3")))
+})
+
+test_that("tplyr_table throws error when passed a bad header argument", {
+  expect_error(tplyr_table(data.frame(), list("a", "b", "c"), c(a = "1", b = "2", c = "3")), class = "tplyr_table_constructor_error")
+  expect_error(tplyr_table(data.frame(), c(1, 2, 3), c(a = "1", b = "2", c = "3")), class = "tplyr_table_constructor_error")
+  expect_silent(tplyr_table(data.frame(), c("a", "b", "c"), c(a = "1", b = "2", c = "3")))
+})
+
+test_that("tplyr_table throws error when passed a bad header_n argument", {
+  expect_error(tplyr_table(data.frame(), c("a", "b", "c"), c("1", "2", "3")), class = "tplyr_table_constructor_error")
+  expect_error(tplyr_table(data.frame(), c("a", "b", "c"), c(a = 1, b = 2, c = 3)))
+  expect_error(tplyr_table(data.frame(), c("a", "b", "c"), c(b = "1", c = "2", d = "3")), class = "tplyr_table_constructor_error")
+  expect_error(tplyr_table(data.frame(), c("a", "b", "c"), c(a = "1", b = "2", c = "3", d = "4")), class = "tplyr_table_constructor_error")
+  expect_silent(tplyr_table(data.frame(), c("a", "b", "c"), c(a = "1", b = "2", c = "3")))
 })
