6b6ea0b1cff29d6287a07643805ea18ecb604d65
mstackhouse <mike.stackhouse@atorusresearch.com>
2020-05-29 20:58:28 +0000

    Initial construction of layer objects attached into table

--- R/layering.R
+++ R/layering.R
@@ -1,2 +1,67 @@
 ### Layering Interfaces
-NULL
+
+#' Attach a layer to a \code{tplyr_table} object
+#'
+#' @param parent
+#' @param layer
+#'
+#' @return
+#' @export
+#'
+#' @examples
+add_layer <- function(parent, layer) {
+
+  # Capture the layer code as a quosure
+  layer <- enquo(layer)
+
+  # Insert the `parent` argument into the topmost call of the layer code
+  # (i.e. if any pipes %>% then pull out the left most call and modify it)
+  l <- modify_nested_call(layer, parent=parent)
+  dmessage(paste('Modified call:', as_label(l)))
+
+  # Evaluate the layer and grab `tplyr_layer` or `tplyr_subgroup_layer` object
+  executed_layer <- eval(quo_get_expr(l))
+
+  # Insert the layer into the parent object
+  parent$layers <- append(parent$layers, executed_layer)
+  parent
+}
+
+#' Create a \code{count} layer for discrete count based summaries
+#'
+#' @param parent
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+group_count <- function(parent, ...) {
+  tplyr_layer(parent, type='count', ...)
+}
+
+#' Create a \code{desc} layer for descriptive statistics summaries
+#'
+#' @param parent
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+group_desc <- function(parent, ...) {
+  tplyr_layer(parent, type='desc', ...)
+}
+
+#' Create a \code{count} layer for shift count summaries
+#'
+#' @param parent
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+group_shift <- function(parent, ...) {
+  tplyr_layer(parent, type='shift', ...)
+}
--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,25 @@
 ### Utility Functions
-NULL
+
+#' Extract the top function from a nested call and insert desired arguments
+#'
+#' @param c An R expression
+#' @param ... Parameters to insert into topmost call
+#'
+#' @return The original call object with
+#'
+#' @examples
+modify_nested_call <- function(c, ...) {
+
+  # If the call is not from magrittr, then modify the contents and return the call
+  if (call_name(c) != "%>%") {
+    c <- call_modify(.call=c, ...)
+    c
+  } else {
+    # Recursively extract the left side of the magrittr call to work your way up
+    e <- call_standardise(c)
+    c <- modify_nested_call(call_args(e)$lhs, ...)
+    # Modfify the magittr call by inserting the call retrieved from recursive command back in
+    c <- call_modify(e, lhs=c)
+    c
+  }
+}
