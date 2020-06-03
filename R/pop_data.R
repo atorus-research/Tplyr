e24b92e8921d31f8f9ad6919bcf7b801844dc51c
Eli Miller <Eli.Miller@AtorusResearch.com>
2020-06-03 14:21:38 +0000

    Add function for add_total_group

--- R/pop_data.R
+++ R/pop_data.R
@@ -25,3 +25,29 @@ default_header_n <- function(table) {
   }, envir = table)
   table
 }
+
+#' Add a treatment group for the entire dataset population
+#'
+#' This function is a wrapper around \code{add_treat_group} and creates a group
+#' named 'Total' containing the set of unique values in the pop_treat_var
+#' variable.
+#'
+#' @param table A \code{tplyr_table} object
+#'
+#' @return The modified table object
+#' @export
+#'
+#' @examples
+#' tab <- tplyr_table(iris, Species)
+#'
+#' add_total_group(tab)
+#' treat_grps(tab)
+#' # Returns "setosa", "verisicolor", and "virginica"
+#'
+#' @rdname treat_grps
+add_total_group <- function(table) {
+  evalq({
+    add_treat_group(current_env(), "Total",
+                    as.character(unique(pop_data[, quo_name(pop_treat_var)])))
+  }, envir = table)
+}
