ba49330f43f232d0f929bdcc86779dffdac930e7
Eli Miller <Eli.Miller@AtorusResearch.com>
2020-06-12 18:55:35 +0000

    Update where to default to TRUE and add assertations

--- R/assertions.R
+++ R/assertions.R
@@ -40,7 +40,7 @@ assert_has_class <- function(x, should_be) {
   param <- enexpr(x)
 
   # Is the argument the class that it shoudl be?
-  if (class(x) != should_be){
+  if (!(should_be) %in% class(x)){
     # Grab the trace back into an object
     trc <- trace_back()
     # Look at the length of the traceback
@@ -202,4 +202,30 @@ unpack_vars <- function(quo_list, allow_character=TRUE) {
 #' @rdname custom_assertions
 is_null_or_call <- function(quo_var) {
   quo_is_null(quo_var) || class(quo_get_expr(quo_var)) == "call"
+}
+
+#' Check if a quosure is null or contains a logical value
+#'
+#' @param quo_var A quosure object to check
+#'
+#' @family Custom Assertions
+#' @rdname custom_assertions
+is_logical_or_call <- function(quo_var) {
+  is_logical(quo_get_expr(quo_var)) || class(quo_get_expr(quo_var)) == "call"
+}
+
+#' @param object Object to check if its a layer
+#'
+#' @family Custom Assertions
+#' @rdname custom_assertions
+assert_is_layer <- function(object) {
+ assert_has_class(object, "tplyr_layer")
+}
+
+#' @param object Object to check if its a layer
+#'
+#' @family Custom Assertions
+#' @rdname custom_assertions
+assert_is_table <- function(object) {
+  assert_has_class(object, "tplyr_table")
 }
