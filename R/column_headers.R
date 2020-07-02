0a55e3a054a472eb6bcfee88f097b636118e4767
mstackhouse <mike.stackhouse@atorusresearch.com>
2020-07-02 17:47:29 +0000

    More work on column headers

--- R/column_headers.R
+++ R/column_headers.R
@@ -1,4 +1,4 @@
-string <- "row label 1 {test}| row label 2 | Spanning header {Inside header1 | Inside header 2} | Spanning header 2 {Inside header1 | Inside header 2}"
+string <- "row label 1 {test | test2}| row label 2 | Spanning header {Inside header1 | Inside header 2} | Spanning header 2 {Inside header1 | Inside header 2}"
 
 
 # Find the inside groups
@@ -28,22 +28,40 @@ for (i in seq_along(op)) {
 ## Build the spanner row ----
 ## Get the spanner headers
 spanner_locs <- str_locate_all(string, "[^{\\|]*\\{")[[1]]
+spanned_sects <- str_locate_all(string,"[^\\{\\|]*\\{[^\\}]*\\}")[[1]]
 
 # Initialize output vector
-spanning_row <- character(nrow(spanner_locs))
+spanning_row <- character(nrow(spanner_locs) + 1)
+bottom_row <- string
+
 for (i in 1:nrow(spanner_locs)) {
   # Pick out the location info
-  locs <- spanner_locs[i, ]
+  top_locs <-spanner_locs[i, ]
+  sect_locs <- spanned_sects[i ,]
+
+
+  # Grab part of the string to count the | from (i.e. the end of the last section)
+  sect_start <- ifelse(i == 1, 1, spanner_locs[i-1, ]['end'])
 
   # Grab the text of the spanner header
-  txt <- str_trim(str_sub(string, locs['start'], locs['end']-1))
+  top_txt <- str_trim(str_sub(string, top_locs['start'], top_locs['end']-1))
+  sect_txt <- str_trim(str_sub(string, sect_locs['start'], sect_locs['end']-1))
+
+  # Count the bars that need to be prefixed to this part of the spanner to split up cells
+  bars <- str_count(str_sub(string, sect_start, top_locs['end']), fixed("|"))
 
   # Prefix with | to pad how many cells are before the text and commit into spanning vector
-  spanning_row[[i]] <- paste(c(rep("|", str_count(str_sub(string, 1, locs['end']), fixed("|"))), txt), collapse="")
-
+  spanning_row[[i]] <- paste(c(rep("|", bars), top_txt), collapse="")
 }
 
-# [^\\{\\|]*\\{[^\\}]*\\}
+# Add in the final number of bars
+bars <- str_count(str_sub(string, top_locs['end']), fixed("|"))
+spanning_row[[i + 1]] <- paste(c(rep("|", bars)), collapse="")
+
+spanner_row_text <- paste(spanning_row, collapse="")
+spanner_row_text
+
+#
 
 
 # Process the outside groups
