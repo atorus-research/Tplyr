f98050ad9ec203b936cf5fdff9e840e87ba5421e
mstackhouse <mike.stackhouse@atorusresearch.com>
2020-07-06 20:10:10 +0000

    Revert update but leave code in place for post alpha update.

--- R/desc.R
+++ R/desc.R
@@ -85,8 +85,9 @@ process_formatting.desc_layer <- function(x, ...) {
       )
 
       # String pad each of the display strings to match the longest value across layers
-      trans_sums[[i]] <- trans_sums[[i]] %>%
-        mutate(display_string = str_pad(display_string, max_layer_length, side='right'))
+      # TODO: Introduce auto-padding after alhpa release
+      # trans_sums[[i]] <- trans_sums[[i]] %>%
+      #   mutate(display_string = str_pad(display_string, max_layer_length, side='right'))
 
       # Now do one more transpose to split the columns out
       # Default is to use the treatment variable, but if `cols` was provided
