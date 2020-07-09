de9bee606422afec8ddca3fa3a650395785db7cd
mstackhouse <mike.stackhouse@atorusresearch.com>
2020-07-09 01:27:11 +0000

    Fix error where cols argument breaks build

--- R/utils.R
+++ R/utils.R
@@ -116,6 +116,7 @@ replace_by_string_names <- function(dat, by) {
 
   # Insert row labels to the front of the tibble
   select(dat, all_of(sort(row_labels)), everything()) %>%
+    ungroup %>%
     mutate_at(row_labels, ~ as.character(.x)) # Coerce all row labels into character
 }
 
