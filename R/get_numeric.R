7f8b44afad3d16b8960f710d7244cf7e086fbbb6
mstackhouse <mike.stackhouse@atorusresearch.com>
2020-08-11 19:33:22 +0000

    Add filtering to get_numeric_data and add get_stats_data

--- R/get_numeric.R
+++ R/get_numeric.R
@@ -4,12 +4,18 @@
 #' a method of accessing that directly.
 #'
 #' When used on a \code{tplyr_table} object, this method will aggregate the numeric data from all Tplyr layers. The data will be
-#' returned to the user in a list of dataframes. If the data has already been processed (i.e. \code{build} has
+#' returned to the user in a list of data frames. If the data has already been processed (i.e. \code{build} has
 #' been run), the numeric data is already available and will simply be returned. Otherwise, the numeric portion of the layer will
 #' be processed.
 #'
+#' Using the layer and where parameters, data for a specific layer can be extracted and subset, making it
+#' simple to directly access data of interest. This is most clear when layers are given text names
+#' instead of using a layer index, but a numeric index works as well.
+#'
 #' @param x A tplyr_table or tplyr_layer object
-#' @param ... Arguments to pass foward
+#' @param layer Layer name or index to select out specifically
+#' @param where Subset criteria passed to dplyr::filter
+#' @param ... Additional arguments to pass forward
 #'
 #' @return Numeric data from the Tplyr layer
 #' @export
@@ -18,16 +24,25 @@
 #' # Load in pipe
 #' library(magrittr)
 #'
-#' tplyr_table(mtcars, gear) %>%
-#'   add_layer(
-#'     group_count(cyl)
-#'   ) %>%
-#'   add_layer(
-#'     group_desc(mpg)
-#'   ) %>%
-#'   get_numeric_data()
+#' t <- tplyr_table(mtcars, gear) %>%
+#'  add_layer(name='drat',
+#'            group_desc(drat)
+#'  ) %>%
+#'  add_layer(name='cyl',
+#'            group_count(cyl)
+#'  )
 #'
-get_numeric_data <- function(x, ...) {
+#'  # Return a list of the numeric dataframes
+#'  get_numeric_data(t)
+#'
+#'  # Get the data from a specific layer
+#'  get_numeric_data(t, layer='drat')
+#'  get_numeric_data(t, layer=2)
+#'
+#'  # Get the data and filter it
+#'  get_numeric_data(t, layer='drat', where = gear==3)
+#'
+get_numeric_data <- function(x, layer=NULL, where=TRUE, ...) {
   UseMethod("get_numeric_data")
 }
 
@@ -35,7 +50,23 @@ get_numeric_data <- function(x, ...) {
 #' Get numeric data from a tplyr_table object
 #' @export
 #' @noRd
-get_numeric_data.tplyr_table <- function(x, ...) {
+get_numeric_data.tplyr_table <- function(x, layer=NULL, where=TRUE, ...) {
+
+  where <- enquo(where)
+
+  # If where is provided then a layer must be specified
+  assert_that(!(quo_get_expr(where) != TRUE && is.null(layer)),
+              msg="If `where` is provided, `layer_name` must be specified")
+
+  # If layer is a numeric value then it must be in range
+  if (is.numeric(layer)) {
+    assert_that(between(layer, 1, length(layers)), msg="Provided layer index is out of range")
+  }
+
+  # The provided name must exist
+  if (!is.null(layer) && !is.null(names(x$layers))) {
+    assert_that(layer %in% names(x$layers), msg=paste("Layer", layer, "does not exist"))
+  }
 
   # If the pre-build wasn't executed then execute it
   if (!'built_target' %in% ls(x)) {
@@ -43,10 +74,14 @@ get_numeric_data.tplyr_table <- function(x, ...) {
     build_header_n(x)
   }
 
-  # Gather the numeric data from each layer
-  num_data <- map(x$layers, get_numeric_data)
+  # If not picking a specific layer, then get all the numeric data
+  if (is.null(layer)) {
+    return(map(x$layers, get_numeric_data))
+  }
 
-  num_data
+  # Otherwise, pick it out and filter
+  get_numeric_data(x$layers[[layer]]) %>%
+    filter(!!where)
 
 }
 
@@ -54,7 +89,7 @@ get_numeric_data.tplyr_table <- function(x, ...) {
 #' Get numeric data from a tplyr_layer object
 #' @export
 #' @noRd
-get_numeric_data.tplyr_layer <- function(x, ...) {
+get_numeric_data.tplyr_layer <- function(x, layer=NULL, where=TRUE, ...) {
 
   # If the numeric data doesn't exist in the layer then process it
   if (!'numeric_data' %in% ls(x)) {
@@ -63,4 +98,139 @@ get_numeric_data.tplyr_layer <- function(x, ...) {
 
   # Return the object
   env_get(x, 'numeric_data')
+}
+
+
+
+#' Get statistics data
+#'
+#' Like the layer numeric data, Tplyr also stores the numeric data produced from
+#' statistics like risk difference. This helper function makes it easy to obtain
+#' that data from the environment
+#'
+#' When used on a \code{tplyr_table} object, this method will aggregate the numeric data from all Tplyr layers
+#' and calculate all statistics. The data will be returned to the user in a list of data frames.
+#' If the data has already been processed (i.e. \code{build} has been run), the numeric data is already
+#' available and the statistic data will simply be returned. Otherwise, the numeric portion of the layer will
+#' be processed.
+#'
+#' Using the layer, where, and statistic parameters parameters, data for a specific layer statistic can be
+#' extracted and subset, making it simple to directly access data of interest. This is most clear when
+#' layers are given text names instead of using a layer index, but a numeric index works as well. If just
+#' a statistic is specified, that statistic will be collected and returned in a list of data frames, allowing you to
+#' grab, for example, just the risk difference statistics across all layers.
+#'
+#' @param x A tplyr_table or tplyr_layer object
+#' @param layer Layer name or index to select out specifically
+#' @param statistic Statistic name or index to select
+#' @param where Subset criteria passed to dplyr::filter
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' t <- tplyr_table(mtcars, gear) %>%
+#'   add_layer(name='drat',
+#'             group_desc(drat)
+#'   ) %>%
+#'   add_layer(name="cyl",
+#'             group_count(cyl)
+#'   ) %>%
+#'   add_layer(name="am",
+#'             group_count(am) %>%
+#'               add_risk_diff(c('4', '3'))
+#'   ) %>%
+#'   add_layer(name="carb",
+#'             group_count(carb) %>%
+#'               add_risk_diff(c('4', '3'))
+#'   )
+#'
+#'  # Returns a list of lists, containing stats data from each layer
+#'  get_stats_data(t)
+#'
+#'  # Returns just the riskdiff statistics from each layer - NULL for layers
+#'  # without riskdiff
+#'  get_stats_data(t, statistic="riskdiff")
+#'
+#'  # Return the statistic data for just the "am" layer - a list
+#'  get_stats_data(t, layer="am")
+#'  get_stats_data(t, layer=2)
+#'
+#'  # Return the riskdiff for the "am" layer - a data frame
+#'  get_stats_data(t, layer="am", statistic="riskdiff")
+#'
+#'  # Return and filter the riskdiff for the am layer - a data frame
+#'  get_stats_data(t, layer="am", statistic="riskdiff", where = summary_var==1)
+#'
+get_stats_data <- function(x, layer=NULL, statistic=NULL, where=TRUE, ...) {
+  UseMethod("get_stats_data")
+}
+
+
+#' Get numeric data from a tplyr_table object
+#' @export
+#' @noRd
+get_stats_data.tplyr_table <- function(x, layer=NULL, statistic=NULL, where=TRUE, ...) {
+
+  where <- enquo(where)
+
+  # If where is provided then a layer must be specified
+  assert_that(!(quo_get_expr(where) != TRUE && (is.null(layer) && is.null(statistic))),
+              msg="If `where` is provided, `layer_name` and `statistic` must be specified")
+
+  # If layer is a numeric value then it must be in range
+  if (is.numeric(layer)) {
+    assert_that(between(layer, 1, length(layers)), msg="Provided layer index is out of range")
+  }
+
+  # The provided name must exist
+  if (!is.null(layer) && !is.null(names(x$layers))) {
+    assert_that(layer %in% names(x$layers), msg=paste("Layer", layer, "does not exist"))
+  }
+
+  # If the pre-build wasn't executed then execute it
+  if (!'built_target' %in% ls(x)) {
+    treatment_group_build(x)
+    build_header_n(x)
+  }
+
+  # If not picking a specific layer, then get all the numeric data
+  if (is.null(layer)) {
+    return(map(x$layers, get_stats_data, statistic=statistic))
+  }
+
+  # Otherwise, pick out the stats container
+  stats <- get_stats_data(x$layers[[layer]], statistic=statistic)
+
+  if (!is.null(statistic)) {
+    stats <- stats %>%
+      filter(!!where)
+  }
+
+  stats
+}
+
+
+#' Get numeric data from a tplyr_layer object
+#' @export
+#' @noRd
+get_stats_data.tplyr_layer <- function(x, layer=NULL, statistic=NULL, where=TRUE, ...) {
+
+  # If the numeric data doesn't exist in the layer then process it
+  if (!'numeric_data' %in% ls(x)) {
+    process_summaries(x)
+  }
+
+  # Return the object
+  stats <- env_get(x, 'stats')
+
+  # Extract the stats_numeric_data object from the environments
+  stats <- map(stats, ~ env_get(env=.x, 'stats_numeric_data'))
+
+  if (!is.null(statistic)) {
+    stats <- stats[[statistic]]
+  }
+
+  stats
 }
---
+++ tests/testthat/test-get_numeric.R
@@ -0,0 +1,143 @@
+context('get_numeric.R')
+
+## Numeric Data ----
+test_that("Error handling - numeric", {
+
+  t <- tplyr_table(mtcars, gear) %>%
+    add_layer(name='drat',
+      group_desc(drat)
+    )
+
+  expect_error(get_numeric_data(t, where=x==1), "If `where`")
+  expect_error(get_numeric_data(t, layer='blah'), regexp="does not exist$")
+  expect_error(get_numeric_data(t, layer=10), "Provided layer index is out of range")
+
+})
+
+t <- tplyr_table(mtcars, gear) %>%
+  add_layer(name='drat',
+            group_desc(drat)
+  ) %>%
+  add_layer(name='cyl',
+            group_count(cyl)
+  )
+
+test_that("Numeric data is pulled from unbuilt table", {
+  # Check that the necessary processing is run by get_numeric_data
+  expect_true(!'built_target' %in% ls(envir = t))
+  get_numeric_data(t)
+  expect_true('built_target' %in% ls(envir = t))
+
+})
+
+test_that("No parameters gives a list of all numeric datasets", {
+  # Check the types of everything
+  dat_list <- get_numeric_data(t)
+  expect_type(dat_list, 'list')
+  expect_s3_class(dat_list[[1]], 'tbl_df')
+  expect_s3_class(dat_list[[2]], 'tbl_df')
+})
+
+test_that("Providing a layer returns a dataframe from that layer", {
+
+  dat <- get_numeric_data(t, layer='drat')
+  expect_equal(dat, t$layers$drat$numeric_data)
+
+})
+
+test_that("Providing a filter returns a filtered dataframe", {
+
+  dat <- get_numeric_data(t, layer='drat', where=gear==3)
+  expect_equal(dat, {
+    t$layers$drat$numeric_data %>%
+      filter(gear==3)
+  }
+  )
+
+})
+
+## Statistic Data ----
+test_that("Error handling - statistic", {
+  t <- tplyr_table(mtcars, gear) %>%
+    add_layer(name="carb",
+              group_count(carb) %>%
+                add_risk_diff(c('4', '3'))
+    )
+
+  expect_error(get_stats_data(t, where=x==1), "If `where`")
+  expect_error(get_stats_data(t, layer='blah'), regexp="does not exist$")
+  expect_error(get_stats_data(t, layer=10), "Provided layer index is out of range")
+
+})
+
+t <- tplyr_table(mtcars, gear) %>%
+  add_layer(name='drat',
+            group_desc(drat)
+  ) %>%
+  add_layer(name="cyl",
+            group_count(cyl)
+  ) %>%
+  add_layer(name="am",
+            group_count(am) %>%
+              add_risk_diff(c('4', '3'))
+  ) %>%
+  add_layer(name="carb",
+            group_count(carb) %>%
+              add_risk_diff(c('4', '3'))
+  )
+
+test_that("Statistics data is pulled from unbuilt table", {
+  # Check that the necessary processing is run by get_numeric_data
+  expect_true(!'built_target' %in% ls(envir = t))
+  suppressWarnings(get_numeric_data(t))
+  expect_true('built_target' %in% ls(envir = t))
+})
+
+test_that("No parameters gives a list of statistics data in a list of layers", {
+  dat_list <- get_stats_data(t)
+
+  # Named list
+  expect_type(dat_list, 'list')
+  expect_named(dat_list, c('drat', 'cyl', 'am', 'carb'))
+  # Elements of list contain a list
+  walk(dat_list, expect_type, type='list')
+  # No stats on first two - empty
+  expect_equivalent(map_int(dat_list, length), c(0, 0, 1, 1))
+  # Last 2 have dataframes
+  expect_s3_class(dat_list[[3]][[1]], 'tbl_df')
+  expect_s3_class(dat_list[[4]][[1]], 'tbl_df')
+
+
+})
+
+test_that("Providing a layer returns a list of statistics from that layer", {
+
+  stats <- get_stats_data(t, layer='drat')
+  expect_equal(stats, list())
+  stats <- get_stats_data(t, layer='am')
+  expect_equal(stats, map(t$layers$am$stats, ~ env_get(env=.x, 'stats_numeric_data')))
+
+})
+
+test_that("Providing a statistic returns a list of dataframes for that statistic", {
+
+  dat_list <- get_stats_data(t, statistic='riskdiff')
+  expect_type(dat_list, 'list')
+  expect_equal(dat_list[[1]], NULL)
+  expect_equal(dat_list[[4]], env_get(env=t$layers$carb$stats$riskdiff, 'stats_numeric_data'))
+
+})
+
+test_that("Providing a layer and a statistic returns a dataframe matching that criteria", {
+  dat <- get_stats_data(t, layer='carb', statistic='riskdiff')
+  expect_equal(dat, env_get(env=t$layers$carb$stats$riskdiff, 'stats_numeric_data'))
+})
+
+test_that("Providing a filter returns a filtered dataframe", {
+
+  dat <- get_stats_data(t, layer='carb', statistic='riskdiff', where=summary_var==1)
+  expect_equal(dat, {
+    env_get(env=t$layers$carb$stats$riskdiff, 'stats_numeric_data') %>%
+      filter(summary_var==1)
+    })
+})
