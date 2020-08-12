87e822815b6e05f586c525c96c5a705fe9e9adb9
Eli Miller <Eli.Miller@AtorusResearch.com>
2020-08-12 19:53:56 +0000

    Updates to nested counts

--- R/count.R
+++ R/count.R
@@ -19,10 +19,6 @@ process_summaries.count_layer <- function(x, ...) {
 
     process_nested_count_target(x)
 
-    # # Begin with the layer itself and process the first target vars values one by one
-    # env_bind(x, numeric_data = map_dfr(unlist(get_target_levels(x, env_get(x, "target_var")[[1]])),
-    #         bind_nested_count_layer, x = x))
-
   } else {
 
     process_single_count_target(x)
@@ -70,7 +66,7 @@ process_single_count_target <- function(x) {
     numeric_data <- summary_stat %>%
       bind_rows(total_stat) %>%
       mutate(!!target_var[[1]] := prefix_count_row(!!target_var[[1]], count_row_prefix)) %>%
-      rename("summary_var" = !!target_var[[1]]) %>%
+      rename("summary_var" = !!tail(target_var, 1)[[1]]) %>%
       group_by(!!!denoms_by) %>%
       do(get_denom_total(., denoms_by, denoms_df)) %>%
       ungroup()
@@ -92,12 +88,37 @@ process_nested_count_target <- function(x) {
     second_layer <- process_summaries(group_count(current_env(), target_var = !!target_var[[2]],
                                                   by = vars(!!target_var[[1]], !!!by), where = !!where))
 
-    numeric_data <- bind_rows(first_layer$numeric_data, second_layer$numeric_data)
-    by <- c(target_var[[1]], by)
+    first_layer_final <- first_layer$numeric_data %>%
+      add_column(!!target_var[[1]] := .[["summary_var"]])
+
+    second_layer_final <- second_layer$numeric_data %>%
+      group_by(!!target_var[[1]]) %>%
+      do(filter_nested_inner_layer(., target, as_name(target_var[[1]]), as_name(target_var[[2]])))
+
+    numeric_data <- bind_rows(first_layer_final, second_layer_final)
+    # Flip the names of the summary_var and the second
+    by <- vars(!!target_var[[1]], !!!by)
     target_var <- vars(!!target_var[[2]])
 
 
   }, envir = x)
+
+}
+
+#' This function is meant to remove the values of an inner layer that don't
+#' appear in the target data
+#' @noRd
+filter_nested_inner_layer <- function(.group, target, outer_name, inner_name) {
+
+  current_outer_value <- unique(.group[, outer_name])[[1]]
+
+  target_inner_values <- target %>%
+    filter(!!sym(outer_name) == current_outer_value) %>%
+    select(inner_name) %>%
+    unlist()
+
+  .group %>%
+    filter(summary_var %in% target_inner_values)
 
 }
 
@@ -268,7 +289,7 @@ process_formatting.count_layer <- function(x, ...) {
 
     if(!is.null(nest_count) && !nest_count) {
       formatted_data <- formatted_data %>%
-        replace_by_string_names(quos(!!target_var[[1]], !!!by, summary_var))
+        replace_by_string_names(quos(!!!by, summary_var))
     } else {
       formatted_data <- formatted_data %>%
         mutate(!!as_name(target_var[[1]]) := NULL) %>%
