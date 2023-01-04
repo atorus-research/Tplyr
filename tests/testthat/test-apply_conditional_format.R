42cc3b383d9e268096013246dc3d16c715c09e00
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2023-01-04 20:11:51 +0000

    Add additional test for numbers with spaces

--- tests/testthat/test-apply_conditional_format.R
+++ tests/testthat/test-apply_conditional_format.R
@@ -1,4 +1,5 @@
 test_string1 <- c(" 0  (0.0%)", " 8  (9.3%)", "78 (90.7%)", "12", "Howdy yall")
+test_string2 <- c(" 0 ( 0.0%)", " 8 ( 9.3%)", "78 (90.7%)", "12", "Howdy yall")
 
 test_that("Test input validation and warning generation", {
   expect_error(
@@ -46,6 +47,11 @@ test_that("Conditional formatting is correctly applied", {
   expect_equal(
     apply_conditional_format(test_string1, 2, x==0, "(<1%)"),
     c(" 0   (<1%)", " 8  (9.3%)", "78 (90.7%)", "12", "Howdy yall")
+  )
+
+  expect_equal(
+    apply_conditional_format(test_string2, 2, x==0, "( <1%)"),
+    c(" 0  ( <1%)", " 8 ( 9.3%)", "78 (90.7%)", "12", "Howdy yall")
   )
 
   expect_equal(
