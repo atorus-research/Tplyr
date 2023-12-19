4a94429f18436238494b1d6a8c66002d2bfff191
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2023-12-19 20:56:13 +0000

    Updated for #170

--- R/replace_leading_whitespace.R
+++ R/replace_leading_whitespace.R
@@ -7,22 +7,28 @@
 #' @export
 #'
 #' @examples
-#' x <- c(" Hello there", "  Goodbye Friend ",  "\tNice to meet you", "  \t What are you up to? \t \t ")
+#' x <- c(" Hello there", "  Goodbye Friend ",  "\tNice to meet you",
+#' "  \t What are you up to? \t \t ")
 #' replace_leading_whitespace(x)
 #'
 #' replace_leading_whitespace(x, tab=2)
 #'
 replace_leading_whitespace <- function(x, tab_width=4) {
   # Pull out the leading whitespace chunk
-  leading_spaces <- stringr::str_match(x, "^([ \\t])+")[,1]
+  leading_spaces <- str_match(x, "^([ \\t])+")[,1]
   # Count spaces and tabs, factor in tab width
-  spaces <- stringr::str_count(leading_spaces, pattern = " ")
-  tabs <- stringr::str_count(leading_spaces, pattern = "\\t") * tab_width
+  spaces <- str_count(leading_spaces, pattern = " ")
+  tabs <- str_count(leading_spaces, pattern = "\\t") * tab_width
   leading_length <- as.integer(spaces + tabs)
 
   # Build the &nbsp; string and combine with the trimmed string
-  nbsp_string <- map_chr(leading_length, \(.x) paste(rep("&nbsp;", .x), collapse=""))
-  minus_whitespace <- stringr::str_trim(x, side=left)
+  nbsp_string <- map_chr(leading_length, \(.x) {
+    if (!is.na(.x)) {
+      paste(rep("&nbsp;", .x), collapse="")
+    } else {
+      ""
+    }})
+  minus_whitespace <- str_trim(x, side='left')
   paste(nbsp_string, minus_whitespace, sep="")
 }
 
